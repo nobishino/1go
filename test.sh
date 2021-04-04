856d9b10e3043aa7a0ca605f9a6dcd5e4b6fbb50
nobishino <nobishii11@gmail.com>
2021-04-04 12:31:35 +0900

    test.shのテストケースにも;を追加する

--- test.sh
+++ test.sh
@@ -17,30 +17,30 @@ assert() {
 }
 go test ./...
 
-assert 0 0
-assert 42 42
-assert 9 4+5
-assert 13 4+21-12
-assert 41 " 12 + 34 - 5 "
-assert 4 " 1*2 + 5 /2"
-assert 47 '5+6*7'
-assert 15 '5*(9-6)'
-assert 4 '(3+5)/2'
-assert 10 '-10+20'
-assert 1 '1==1'
-assert 0 '1==2'
-assert 0 '1!=1'
-assert 1 '1!=2'
-assert 1 '1<2'
-assert 0 '1<1'
-assert 0 '2<=1'
-assert 1 '1<=2'
-assert 1 '1<=1'
-assert 0 '1>1'
-assert 1 '2>1'
-assert 1 '2>=1'
-assert 1 '1>=1'
-assert 0 '0>=1'
-assert 1 'a=1'
+assert 0 "0;"
+assert 42 "42;"
+assert 9 "4+5;"
+assert 13 "4+21-12;"
+assert 41 " 12 + 34 - 5 ;"
+assert 4 " 1*2 + 5 /2;"
+assert 47 '5+6*7;'
+assert 15 '5*(9-6);'
+assert 4 '(3+5)/2;'
+assert 10 '-10+20;'
+assert 1 '1==1;'
+assert 0 '1==2;'
+assert 0 '1!=1;'
+assert 1 '1!=2;'
+assert 1 '1<2;'
+assert 0 '1<1;'
+assert 0 '2<=1;'
+assert 1 '1<=2;'
+assert 1 '1<=1;'
+assert 0 '1>1;'
+assert 1 '2>1;'
+assert 1 '2>=1;'
+assert 1 '1>=1;'
+assert 0 '0>=1;'
+assert 1 'a=1;'
 
 echo OK
